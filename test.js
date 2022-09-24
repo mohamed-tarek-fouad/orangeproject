68f0ae73fe8e37f62bb207dde4090fb62ad13abf
mohamed tarek <mt1348@fayoum.edu.eg>
2022-09-24 19:24:08 +0200

    fixed

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var http = require('http');
+
 const express=require("express")
 const bodyParser=require("body-parser")
 const app=express()
@@ -7,15 +7,15 @@ let users=[]
 app.use(bodyParser.json())
 app.post("/register",(req,res)=>{
     const {firstname,password}=req.body
-    users.bush({firstname,password})
+    users.push({firstname,password})
     res.status(200).json({
         "messege":"register success",
         "Data":users
     })
 })
 app.post("/login",(req,res)=>{
-    let {firstname,password}=req.body
-    if({firstname,password}==users[0]){
+    let {firstname2,password2}=req.body
+    if(firstname2==users.firstname && password2==users.password){
         res.status(200).json({
             "messege":"sucessfull login",
             "Data":users
